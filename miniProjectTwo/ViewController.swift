b1a50ac3b823f65e002146c223145d33daa19ae0
mercedes <mercedes.moore720@gmail.com>
2020-08-12 09:42:47 -0700

    testing one

---
+++ miniProjectTwo/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  miniProjectTwo
+//
+//  Created by Mercedes Moore on 8/7/20.
+//  Copyright © 2020 Mercedes Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    //Image One
+    
+    @IBOutlet weak var openingImage: UIImageView!
+    
+    //Button One
+    
+    
+    @IBAction func startButton(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ miniProjectTwo/viewControllerTwo.swift
@@ -0,0 +1,30 @@
+//
+//  viewControllerTwo.swift
+//  miniProjectTwo
+//
+//  Created by Mercedes Moore on 8/7/20.
+//  Copyright © 2020 Mercedes Moore. All rights reserved.
+//
+
+import UIKit
+
+class viewControllerTwo: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
